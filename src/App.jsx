7dc3888fb36c44831f7424cfc76ad8cc1d32fce9
lucaimbalzano <lucaimbalzano@gmail.com>
2024-03-12 16:43:19 +0100

    too much delay --> HoverSection

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ const App = () => (
       <div className={`${styles.boxWidth}`}>
         <Stack />
         <Clients />
-        <HoverSection />
+        {/* <HoverSection /> */}
         <TerminalSimulator />
         <Footer />
       </div>
