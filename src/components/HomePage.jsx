18e2e64f368fecc8cac7da3fd9763c76f0c133fc
Lucas <lucaimbalzano@gmail.com>
2025-03-22 16:26:27 +0100

    fixed text desktop size in homepage hero

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,21 +9,22 @@ const HomePage = () => {
       className={`flex md:flex-row flex-col h-screen ${styles.paddingY}`}
     >
       <div
-        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 xss:space-y-8`}
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6 space-y-4 md:space-y-6`}
       >
-        <div className="flex flex-row items-center py-[6px] px-4  rounded-[10px] mb-2"></div>
+        <div className="flex flex-row items-center py-[6px] px-4 rounded-[10px] mb-2"></div>
 
         <div className="flex flex-row justify-between items-center w-full pt-10">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[90px] xs:text-[120px] xxs:text-[65px] text-[42px] text-white 2xl:leading-[130.8px] ss:leading-[110.8px] xs:leading-[110.8px] leading-[59px] ">
+          <h1 className="flex-1 font-poppins font-semibold text-[42px] sm:text-[65px] md:text-[72px] lg:text-[90px] text-white leading-[1.2] md:leading-[1.15]">
             Impact the <br className="sm:block hidden" />{" "}
-            <span className="text-gradient xs:text-[120px]">future</span>{" "}
+            <span className="text-gradient">future</span>{" "}
           </h1>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[68px] xxs:text-[65px] text-[80px] text-white ss:leading-[100.8px] xxs:leading-[55px] leading-[45px] w-full">
+        <h1 className="font-poppins font-semibold text-[42px] sm:text-[65px] md:text-[72px] lg:text-[90px] text-white leading-[1.2] md:leading-[1.15] w-full">
           Just focus on the present.
         </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        
+        <p className={`${styles.paragraph} max-w-[470px] text-base md:text-lg lg:text-xl mt-2`}>
           For where your treasure is, there your heart will be also.
         </p>
       </div>
@@ -33,7 +34,7 @@ const HomePage = () => {
           scene="https://prod.spline.design/Y2G4uvj5Ge3Sd0XL/scene.splinecode"
           className="hidden md:block"
         />
-        <img src={heartImg} className="md:hidden object-cover w-full h-full" />
+        <img src={heartImg} alt="Heart" className="md:hidden object-cover w-full h-full" />
       </div>
     </section>
   );
